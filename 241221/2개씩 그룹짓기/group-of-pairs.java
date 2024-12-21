ded37599d9ae8b9e99772a149b2695f5e86a8b9e
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2024-12-21 16:22:19 +0900

    Title: 2개씩 그룹짓기, Time: 184ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241221/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] input = new int[N*2];
+
+        for (int i = 0; i < N*2; i++) {
+            input[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(input);
+        int max=0;
+        for(int i=0; i<N; i++){
+            if(max < input[i] + input[N*2-i-1])
+                max = input[i] + input[N*2-i-1];
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
