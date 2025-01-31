95880eaa1baf0de631a2d50341b6b6332e49aae0
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-01 02:16:37 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 250201/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250201/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int offset = 1000;
         Scanner sc = new Scanner(System.in);
-        int[][] grid = new int[offset*2+1][offset*2+1];
+        int[][] grid = new int[offset * 2 + 1][offset * 2 + 1];
         int x1, x2, y1, y2;
         int x3, x4, y3, y4;
+
+        // 입력받은 좌표들
         x1 = sc.nextInt() + offset;
         y1 = sc.nextInt() + offset;
         x2 = sc.nextInt() + offset;
@@ -16,35 +18,43 @@ public class Main {
         y3 = sc.nextInt() + offset;
         x4 = sc.nextInt() + offset;
         y4 = sc.nextInt() + offset;
-        for(int j = x1; j < x2; j++ ){
-            for(int k = y1; k < y2; k++){
+
+        // 첫 번째 직사각형 그리기
+        for (int j = x1; j < x2; j++) {
+            for (int k = y1; k < y2; k++) {
                 grid[j][k] = 1;
             }
         }
-        for(int j = x3; j < x4; j++ ){
-            for(int k = y3; k < y4; k++){
-                if(grid[j][k] == 1)
+
+        // 두 번째 직사각형 빼기
+        for (int j = x3; j < x4; j++) {
+            for (int k = y3; k < y4; k++) {
+                if (grid[j][k] == 1)
                     grid[j][k] = 0;
             }
         }
-        int minX = offset*2+1, maxX = offset, minY = offset*2+1, maxY= offset;
+
+        // 남은 영역의 경계값 계산
+        int minX = offset * 2 + 1, maxX = -1, minY = offset * 2 + 1, maxY = -1;
 
         boolean hasRemaining = false;
-        for(int i = x1; i < x2; i++){
-            for(int j = y1; j < y2; j++){
-                if(grid[i][j] == 1){
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++) {
+                if (grid[i][j] == 1) {
                     hasRemaining = true;
-                    if(grid[i-1][j] == 0) { if(i < minX) minX = i;}
-                    else if (grid[i-1][j] == 1) { if(i > maxX) maxX = i;}
-                    if(grid[i][j-1] == 0) { if( j < minY) minY = j;}
-                    else if (grid[i][j-1] == 1) { if(j > maxY) maxY = j;}
+                    if (i < minX) minX = i;
+                    if (i > maxX) maxX = i;
+                    if (j < minY) minY = j;
+                    if (j > maxY) maxY = j;
                 }
             }
         }
+
+        // 남은 영역의 넓이 계산
         int result = 0;
-        
-        if(hasRemaining == false) result = 0; 
-        else { result = (maxX - minX +1) * (maxY - minY +1); }
+        if (hasRemaining) {
+            result = (maxX - minX + 1) * (maxY - minY + 1);
+        }
 
         System.out.println(result);
         sc.close();
