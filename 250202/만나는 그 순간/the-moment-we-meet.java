fab940dc21f804001d410a046a749a4236cbb8c8
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-02 02:10:25 +0900

    Title: 만나는 그 순간, Time: 232ms, Memory: 29MB, Status: Passed - CodeTree

--- 250202/만나는 그 순간/the-moment-we-meet.java
+++ 250202/만나는 그 순간/the-moment-we-meet.java
@@ -1,48 +1,39 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
+    public static final int MAX_T = 1000000;
+    public static int[] posA = new int[MAX_T+1];
+    public static int[] posB = new int[MAX_T+1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        List<String> moveA = new ArrayList<>();
-        List<String> moveB = new ArrayList<>();
+        int timeA = input(n, sc, posA);
+        int timeB = input(m, sc, posB);
+        int time = Math.max(timeA, timeB);
 
-        for(int i = 0; i < n; i++){
-            moveA.add(sc.next() + " " + sc.next());
-        }
-        for(int i = 0; i < m; i++){
-            moveB.add(sc.next() + " " + sc.next());
-        }
-
-        List<Integer> posA = getPos(moveA);
-        List<Integer> posB = getPos(moveB);
-        int time  = Math.min(posA.size(), posB.size());
-
-        for(int t = 0; t < time; t++){
-            if(posA.get(t).equals(posB.get(t))){
-                System.out.print(t+1);
+        for(int t = 1; t < time; t++){
+            if(posA[t] == posB[t]){
+                System.out.print(t);
                 return;
             }
         }
         System.out.print(-1);
     }
-    private static List<Integer> getPos(List<String> move){
-        List<Integer> positions = new ArrayList<>();
-        int pos = 0;
-        for(String m: move){
-            String[] input = m.split(" ");
-            int dir = input[0].equals("R") ? 1 : -1;   //오른쪽방향이면 1
-            int time = Integer.parseInt(input[1]);
 
-            for(int i = 0; i < time; i++){
-                pos += dir;
-                positions.add(pos);
+    private static int input(int moves, Scanner sc, int[] pos){
+        int curTime = 1;
+        for(int i = 0; i < moves; i++){
+            char dir = sc.next().charAt(0);
+            int time = sc.nextInt();
+            for(int t=curTime; t < curTime + time; t++){
+                pos[t] = pos[t - 1] + (dir == 'R' ? 1 : -1);
             }
+            curTime+=time;
         }
-        return positions;
+        return curTime - 1;
+
     }
 }
