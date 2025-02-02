e5a616c9c5275e3680a644d2a9f7cc558b50e71b
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-03 00:03:01 +0900

    Title: 좌우로 움직이는 로봇, Time: 1043ms, Memory: 58MB, Status: Passed - CodeTree

---
+++ 250203/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int MAX_T = 1000000;
+    public static int[] posA = new int[MAX_T+1];
+    public static int[] posB = new int[MAX_T+1];
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int curA = 1; int curB = 1;
+
+        for(int i = 0; i < n; i++){
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            for(int time = curA; time < curA + t; time++){
+                posA[time] = posA[time-1] + (d=='L' ? -1 : 1);
+            }
+            curA += t;
+        }
+        for(int i = 0; i < m; i++){
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            for(int time = curB; time < curB + t; time++){
+                posB[time] = posB[time-1] + (d=='L' ? -1 : 1);
+            }
+            curB += t;
+        }
+        int cnt = 0;
+        int maxTime = Math.min(Math.max(curA, curB), MAX_T);
+
+        for (int c = curB; c <= maxTime; c++) {
+            posB[c] = posB[c - 1];
+        }
+        for (int c = curA; c <= maxTime; c++) {
+            posA[c] = posA[c - 1];
+        }
+
+        for(int i = 1; i<=maxTime; i++){
+            if(posA[i] == posB[i] && posA[i-1] != posB[i-1]){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
