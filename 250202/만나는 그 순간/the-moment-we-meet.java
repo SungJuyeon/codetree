d38488f5402fc50d868d80acea41063186e7ce34
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-02 17:54:35 +0900

    Title: 만나는 그 순간, Time: 215ms, Memory: 27MB, Status: Passed - CodeTree

--- 250202/만나는 그 순간/the-moment-we-meet.java
+++ 250202/만나는 그 순간/the-moment-we-meet.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_T = 1000000;
-    public static int[] posA = new int[MAX_T+1];
-    public static int[] posB = new int[MAX_T+1];
-
+    public static int MAT_T = 1000000;
+    public static int[] posA = new int[MAT_T + 1];
+    public static int[] posB = new int[MAT_T + 1];
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        int curTimeA = 1;
+        int curTimeB = 1;
 
-        int timeA = input(n, sc, posA);
-        int timeB = input(m, sc, posB);
-        int time = Math.max(timeA, timeB);
+        for(int i = 0; i < n; i++){
+            char c = sc.next().charAt(0);
+            int time = sc.nextInt();
 
-        for(int t = 1; t < time; t++){
+            for(int t = curTimeA; t < curTimeA + time; t++){
+                posA[t] = posA[t-1] + (c=='L' ? -1 : 1);
+            }
+            curTimeA += time;
+        }
+
+        for(int i = 0; i < m; i++){
+            char c = sc.next().charAt(0);
+            int time = sc.nextInt();
+
+            for(int t = curTimeB; t < curTimeB + time; t++){
+                posB[t] = posB[t-1] + (c=='L' ? -1 : 1);
+            }
+            curTimeB += time;
+        }
+        int minTime = Math.min(curTimeA, curTimeB);
+        for(int t = 1; t < minTime; t++){
             if(posA[t] == posB[t]){
                 System.out.print(t);
                 return;
@@ -22,18 +40,5 @@ public class Main {
         }
         System.out.print(-1);
     }
-
-    private static int input(int moves, Scanner sc, int[] pos){
-        int curTime = 1;
-        for(int i = 0; i < moves; i++){
-            char dir = sc.next().charAt(0);
-            int time = sc.nextInt();
-            for(int t=curTime; t < curTime + time; t++){
-                pos[t] = pos[t - 1] + (dir == 'R' ? 1 : -1);
-            }
-            curTime+=time;
-        }
-        return curTime - 1;
-
-    }
 }
+
