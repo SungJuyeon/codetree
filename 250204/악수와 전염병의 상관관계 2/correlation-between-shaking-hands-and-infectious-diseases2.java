0628af4a6596f1a0e639835ef523e8f8df904a5c
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-04 11:15:40 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 169ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 250204/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static int MAX_T = 250;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int p = sc.nextInt();
+        int t = sc.nextInt();
+
+        int[] result = new int[n+1];
+        result[p] = 1;
+        int k_cnt = 0;
+        int[] xTime = new int[MAX_T];
+        int[] yTime = new int[MAX_T];
+
+        for(int i = 0; i < t; i++){
+            int time = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            xTime[time] = x;
+            yTime[time] = y;
+        }
+
+        for(int j = 0; j < MAX_T; j++){
+            if(k_cnt == k) {
+                for(int i = 1; i < n+1; i++){
+                    System.out.print(result[i]);
+                }
+                return;
+            }
+            
+            if(result[xTime[j]] == 1 && result[yTime[j]] == 0) {
+                result[yTime[j]] = 1;
+                k_cnt++;
+            } else if(result[xTime[j]] == 0 && result[yTime[j]] == 1){
+                result[xTime[j]] = 1;
+                k_cnt++;
+            }
+            
+        }
+        
+        for(int i = 1; i < n+1; i++){
+            System.out.print(result[i]);
+        }
+        sc.close();
+    }
+}
