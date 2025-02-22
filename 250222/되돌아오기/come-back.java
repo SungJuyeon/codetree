016ef0af10c48c34b6bb94c07c476576e62f8940
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-22 16:30:21 +0900

    Title: 되돌아오기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250222/되돌아오기/come-back.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int[] arrx = {-1, 0, 0, 1};
+    private static int[] arry = {0, -1, 1, 0};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x=0, y=0;
+        int posit = 0;
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            char pos = sc.next().charAt(0);
+            int mov = sc.nextInt();
+
+            if(pos == 'W'){ //서
+                posit = 0;
+            } else if(pos == 'S') { //남
+                posit = 2;
+            }else if(pos == 'N'){ //북
+                posit = 3;
+            } else {    //E 동
+                posit = 1;
+            }
+
+            for(int t=0; t<mov; t++){
+                x += arrx[posit];
+                y += arrx[posit];
+
+                if(x == 0 && y == 0){
+                    System.out.print(result);
+                    return;
+                }
+                result++;
+            }
+        }
+        System.out.print("-1");
+        sc.close();
+    }
+}
