b674a15a532b363c87e659eaf61d713f5bac9212
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-02-23 14:11:34 +0900

    Title: 이상한 진수 2, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

--- 250223/이상한 진수 2/awkward-digits-2.java
+++ 250223/이상한 진수 2/awkward-digits-2.java
@@ -6,12 +6,16 @@ public class Main {
         String in = sc.next();
         String[] input = in.split("");
         int result = 0;
+        boolean change = false;
         for(int i = 0; i < input.length; i++){
             if(input[i].equals("0")){
                 input[i] = "1";
+                change = true;
                 break;
             }
         }
+        if(change == false) input[input.length - 1] = "0";
+        
         for(int j = 0; j < input.length; j++){
             if(input[j].equals("1")){
                 result += Math.pow(2, (input.length-j-1));
