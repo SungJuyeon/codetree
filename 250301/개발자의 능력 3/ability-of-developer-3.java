3be252206a26ffe114d02eb5a9cfa9e26602b8de
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-03-01 14:42:19 +0900

    Title: 개발자의 능력 3, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250301/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ability = new int[6];
+        for (int i = 0; i < 6; i++) {
+            ability[i] = sc.nextInt();
+        }
+        int min = 100;
+        int sum = 0;
+        for(int i = 0; i<6; i++){
+            sum += ability[i];
+        }
+
+        for(int i =0; i<6; i++){
+            for(int j=i+1; j <6; j++){
+                for(int k = j+1; k<6; k++){
+                    if(i==j || j == k || k == i)
+                        continue;
+                    int one = ability[i] + ability[j] + ability[k];
+                    int two = sum-one;
+                    min = Math.min(min, Math.abs(one-two));
+                }
+            }
+        }
+        System.out.print(min);
+        sc.close();
+    }
+}
