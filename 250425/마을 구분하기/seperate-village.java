f31c082fb2d16f855be17c40126fe2d89709f013
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-04-25 16:58:06 +0900

    Title: 마을 구분하기, Time: 195ms, Memory: 11MB, Status: Passed - Codetree

--- 250425/마을 구분하기/seperate-village.java
+++ 250425/마을 구분하기/seperate-village.java
@@ -7,6 +7,7 @@ public class Main {
     public static int n = 0;
     public static boolean[][] visited;
     public static int[][] grid;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -16,41 +17,37 @@ public class Main {
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
 
-        List<Integer> villageList = new ArrayList<>();
-        
+        List<Integer> list = new ArrayList<>();
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1 && !visited[i][j]){
-                    answer = 0;
-                    DFS(i,j);
-                    villageList.add(answer);
+                    answer = 0; 
+                    DFS(i, j);
+                    list.add(answer);
                 }
             }
         }
-        
-        Collections.sort(villageList);
-        System.out.println(villageList.size());
-        for(int c: villageList){
-            System.out.println(c);
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(int i : list){
+            System.out.println(i);
         }
 
     }
     public static void DFS(int x, int y){
         visited[x][y] = true;
         answer++;
-        
-        for(int i = 0; i < 4; i++){
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            if(isRange(newX, newY) && !visited[newX][newY] && grid[newX][newY] == 1)
-                DFS(newX, newY);
+
+        for(int i = 0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(isRange(nx, ny) && !visited[nx][ny] && grid[nx][ny] ==1) {
+                DFS(nx, ny);
+            }
         }
     }
-
     public static boolean isRange(int x, int y){
-        if(x < 0 || x >= n) return false;
-        if(y < 0 || y >= n) return false;
-        return true;
+        return x >= 0 && y >= 0 && x < n && y < n;
     }
-
 }
