f90f455786c4e892016ff5f3bb7942ebd3d8e031
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-07-16 21:55:54 +0900

    Title: 사각형 채우기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250716/사각형 채우기/rectangle-fill.java
+++ 250716/사각형 채우기/rectangle-fill.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static int n;
+    public static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt(); //세로 크기 = n, 가로 크기 = 2로 고정
         
-        System.out.print(b(n));
+        dp = new int[n+1];
+        b(n);
+        System.out.println(dp[n]);
         
     }
     public static int b(int n) {
@@ -13,11 +16,10 @@ public class Main {
         if(n == 1) return 1;
         if(n == 2) return 2;
 
-        int[] dp = new int[n+1];
         dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
         return dp[n];
     }
