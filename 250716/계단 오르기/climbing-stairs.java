ad9a91ac60f79b491b59930ff7d1571b81fdf38b
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-07-16 21:32:45 +0900

    Title: 계단 오르기, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

--- 250716/계단 오르기/climbing-stairs.java
+++ 250716/계단 오르기/climbing-stairs.java
@@ -1,28 +1,39 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
-    public static long[] list;
+    public static int[] list;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        list = new long[n+1];
-        Arrays.fill(list, -1);
+        list = new int[Math.max(n + 1, 4)];
+        //Arrays.fill(list, -1);
 
-        System.out.print(step(n));
+        list[0] = 0;
+        list[1] = 0;
+        list[2] = 1;
+        list[3] = 1;
+        System.out.print(find(n));
     }
 
-    public static long step(int n) {
-        if(n < 0) return 0;
-        else if(n == 0) return 1;
-        else if( n == 1 ) return 0;
-        else if(n == 2 || n == 3) return 1;
+    // public static int step(int n) {
+    //     if(n < 0) return 0;
+    //     else if(n == 0) return 1;
+    //     else if( n == 1 ) return 0;
+    //     else if(n == 2 || n == 3) return 1;
         
-        if(list[n] != -1) {
-            return list[n];
-        } 
-        return list[n] = (step(n-2) + step(n-3)) % 10007;
+    //     if(list[n] != -1) {
+    //         return list[n];
+    //     } 
+    //     return list[n] = (step(n-2) + step(n-3)) % 10007;
         
+    // }
+
+    private static int find(int n) {
+        for (int i = 4; i <= n; i++) {
+            list[i] = (list[i - 2] + list[i - 3]) % 10007;
+        }
+        return list[n];
     }
 }
