00ed0d0d1a523e564fe4c3ccbf02cb74a0f6a812
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-08-01 17:34:18 +0900

    Title: 거품 정렬 구현, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

--- 250801/거품 정렬 구현/implement-bubble-sort.java
+++ 250801/거품 정렬 구현/implement-bubble-sort.java
@@ -8,16 +8,34 @@ public class Main {
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length-i-1; j++) {
-                if(arr[j] > arr[j+1]){
-                    int tem = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tem;
+        // for(int i = 0; i < arr.length; i++){
+        //     for(int j = 0; j < arr.length-i-1; j++) {
+        //         if(arr[j] > arr[j+1]){
+        //             int tem = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = tem;
+        //         }
+        //     }
+        // }
+        // for (int i = 0; i < n; i++)
+        //     System.out.print(arr[i] + " ");        
+
+        boolean sorted = false;
+
+        while(!sorted) {
+            sorted = true;
+            for(int i = 0; i < arr.length - 1; i++) {
+                if(arr[i] > arr[i+1]){
+                    int tem = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = tem;
+                    sorted= false;
                 }
             }
         }
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");        
+
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
