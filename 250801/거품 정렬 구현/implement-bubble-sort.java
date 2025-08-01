b755036c0b120c7b5ab63638d098164552f136aa
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-08-01 17:27:23 +0900

    Title: 거품 정렬 구현, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250801/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int tem = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tem;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");        
+    }
+}
