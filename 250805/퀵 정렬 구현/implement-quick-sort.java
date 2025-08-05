28e139f599e684b990ca7369f7abc5a76e0ca408
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-08-05 17:42:57 +0900

    Title: 퀵 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250805/퀵 정렬 구현/implement-quick-sort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        quickSort(arr, 0, n-1);
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j = low; j < high; j++) {
+            if(arr[i] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, high);
+        return i+1;
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
