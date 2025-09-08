94bde2cf47827a96c840be1349b0baa286ee07b0
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-09-08 15:17:42 +0900

    Title: 계단 오르기 2, Time: 147ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250908/계단 오르기 2/climbing-stairs-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static int MAXN = 1005;
+    public static int n;
+    public static int[] coins = new int[MAXN];
+    public static int[][] dp = new int[MAXN][5];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        
+        dp[1][1] = coins[1];
+        dp[2][0] = coins[2];
+        dp[2][2] = coins[2] + coins[1];
+
+        for(int i = 3; i <= n; i++) {
+            for(int j = 0; j <=3; j++) {
+                if(dp[i-2][j] != 0) 
+                    dp[i][j] = Math.max(dp[i][j], dp[i-2][j] + coins[i]);
+                if(j>0 && dp[i-1][j-1] != 0)
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + coins[i]);
+            }
+        }
+
+        int ans = 0;
+        for(int j = 0; j <= 3; j++)
+            ans = Math.max(ans, dp[n][j]);
+        
+        System.out.println(ans);
+    }
+
+}
