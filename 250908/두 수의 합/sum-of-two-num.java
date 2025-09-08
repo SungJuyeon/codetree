f69088ae0f19dccec3f2f0e85e2f7ecb99c2c67a
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-09-08 17:40:10 +0900

    Title: 두 수의 합, Time: 814ms, Memory: 36MB, Status: Passed - Codetree

--- 250908/두 수의 합/sum-of-two-num.java
+++ 250908/두 수의 합/sum-of-two-num.java
@@ -7,17 +7,14 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         HashMap<Integer, Integer> m= new HashMap<>();
+        long ans = 0;
 
         for (int i = 1; i <= n; i++) {
-            m.put(i, sc.nextInt());
-        }
-        
-        int ans = 0;
-        for(int j = 1; j <= n; j++) {
-            int find = k - m.get(j);
-            for(int kj = j+1; kj <=n; kj++) {
-                if(m.get(kj) == find) ans++;
+            int x = sc.nextInt();
+            if(m.containsKey(k-x)) {
+                ans += m.get(k-x);
             }
+            m.put(x, m.getOrDefault(x, 0) + 1);
         }
         System.out.println(ans);
     }
