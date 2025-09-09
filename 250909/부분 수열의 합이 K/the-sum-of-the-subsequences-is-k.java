d899df6cb0e2539f07540d87d29e7bf652bf17a7
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-09-09 11:59:59 +0900

    Title: 부분 수열의 합이 K, Time: 197ms, Memory: 12MB, Status: Passed - Codetree

--- 250909/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
+++ 250909/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
 public class Main {
+    public static int[] arr;
+    public static int[] arrSum;
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[n];
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         int ans = 0;
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
-            for(int j = i; j < n; j++) {
-                sum += arr[j];
-                if(sum == k) {
-                    ans++;
-                    break;
-                } else if(sum > k) {
-                    break;
-                }
+        // for(int i = 0; i < n; i++) {
+        //     int sum = 0;
+        //     for(int j = i; j < n; j++) {
+        //         sum += arr[j];
+        //         if(sum == k) {
+        //             ans++;
+        //             break;
+        //         } else if(sum > k) {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        arrSum = new int[n+1];
+        arrSum[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            arrSum[i] = arrSum[i-1] + arr[i-1];
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = i; j <= n; j++) {
+                if(getSum(i,j) == k) ans++;
             }
         }
 
         System.out.println(ans);
+    }
+
+    public static int getSum(int i, int j) {
+        return arrSum[j] - arrSum[i-1];
     }
 }
