c3c7db52139f88ed0dfdf3d623b7615b3dd3ed98
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-09-15 18:15:41 +0900

    Title: 앞에서부터 삭제하기 2, Time: 792ms, Memory: 33MB, Status: Passed - Codetree

--- 250915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -5,24 +5,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int sum = 0;
-        double maxAvg = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         
-        pq.add(arr[n-1]);
-        sum += arr[n-1];
+        // int sum = 0;
+        // double maxAvg = 0;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        // pq.add(arr[n-1]);
+        // sum += arr[n-1];
 
-        for(int i = n-2; i >= 1; i--) {
-            pq.add(arr[i]);
-            sum += arr[i];
+        // for(int i = n-2; i >= 1; i--) {
+        //     pq.add(arr[i]);
+        //     sum += arr[i];
 
-            double avg = (double) (sum - pq.peek()) / (n-i-1);
-            if(maxAvg < avg) maxAvg = avg;
+        //     double avg = (double) (sum - pq.peek()) / (n-i-1);
+        //     if(maxAvg < avg) maxAvg = avg;
+        // }
+
+        long[] prefix = new long[n+1];
+        for(int i = 0; i < n; i++)
+            prefix[i+1] = prefix[i] + arr[i];
+
+        int[] suffixMin = new int[n];
+        suffixMin[n-1] = arr[n-1];
+        for(int i = n-2; i >=1; i--) {
+            suffixMin[i] = Math.min(arr[i], suffixMin[i+1]); 
+        }
+
+        double maxAvg = 0;
+        for(int k = 1; k <= n-2; k++) {
+            long totalSum = prefix[n] - prefix[k];
+            int minVal = suffixMin[k];
+            long sumWithoutMin = totalSum - minVal;
+            int size = n-k-1;
+            double avg = (double) sumWithoutMin / size;
+            maxAvg = Math.max(maxAvg, avg);
         }
 
         System.out.printf("%.2f", maxAvg);
-
     }
 }
