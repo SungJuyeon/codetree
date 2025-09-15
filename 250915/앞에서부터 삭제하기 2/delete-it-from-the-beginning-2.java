e2e14e4298c629c672c9905f5b0684566b15a9fa
SoungJuyeon <144316432+SungJuyeon@users.noreply.github.com>
2025-09-15 17:14:58 +0900

    Title: 앞에서부터 삭제하기 2, Time: 781ms, Memory: 34MB, Status: Passed - Codetree

--- 250915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        int sum = 0;
+        double maxAvg = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         
-        // for(int i = n-2; i > 0; i--) {
-        //     if(arr[i]==)
-        //     PriorityQueue<Integer> pq = new PriorityQueue<>();
-        // }
+        pq.add(arr[n-1]);
+        sum += arr[n-1];
 
-        while(n-2 > 0) {
-            for(int i = 1; i <= n-2; i++) {
-                pa.add(arr[i]);
-            }
-            
+        for(int i = n-2; i >= 1; i--) {
+            pq.add(arr[i]);
+            sum += arr[i];
+
+            double avg = (double) (sum - pq.peek()) / (n-i-1);
+            if(maxAvg < avg) maxAvg = avg;
         }
+
+        System.out.printf("%.2f", maxAvg);
 
     }
 }
